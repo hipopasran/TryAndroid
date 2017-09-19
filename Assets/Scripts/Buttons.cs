1033195add9ee19d7d8aa5d80be1436709a24c41
hipopasran <vladimirkeda@yandex.ru>
2017-09-19 19:24:14 +0300

    menu

--- Assets/Scripts/Buttons.cs
+++ Assets/Scripts/Buttons.cs
@@ -4,14 +4,29 @@ using UnityEngine;
 
 public class Buttons : MonoBehaviour {
 
-    public Sprite layer_one, layer_two;
-    private bool pause = false;
+    
+    public bool pause = false;
     //public string action;
-
+    public GameObject resume,restart,pause1,home,rating,pause_text;
+   
+    
+    void Update()
+    {
+        if(Time.timeScale == 0)
+        {
+            pause = true;
+           
+        }
+        else
+        {
+            pause = false;
+            
+        }
+    }
 	void OnMouseDown()
     {
 
-        GetComponent<SpriteRenderer>().sprite = layer_two;
+        
 
     }
 
@@ -19,7 +34,7 @@ public class Buttons : MonoBehaviour {
     {
         if (gameObject.name != "pause")
         {
-            GetComponent<SpriteRenderer>().sprite = layer_one;
+            
         }
     }
 
@@ -33,18 +48,39 @@ public class Buttons : MonoBehaviour {
             case "rating":
                 Application.OpenURL("http://google.com");
                 break;
+            case "home":
+                Application.LoadLevel("main");
+                break;
+            case "restart":
+                Application.LoadLevel(Application.loadedLevel);
+                break;
+            case "resume":
+                Time.timeScale = 1;
+                pause_text.SetActive(false);
+                pause = false;
+                resume.SetActive(false);
+                restart.SetActive(false);
+                home.SetActive(false);
+                break;
             case "pause":
                 if (pause == false)
                 {
                     Time.timeScale = 0;
-                    GetComponent<SpriteRenderer>().sprite = layer_two;
+                    pause_text.SetActive(true);
                     pause = true;
+                    resume.SetActive(true);
+                    restart.SetActive(true);
+                    home.SetActive(true);
                 }
                else if(pause==true)
                 {
                     Time.timeScale = 1;
-                    GetComponent<SpriteRenderer>().sprite = layer_one;
+                    
                     pause = false;
+                    resume.SetActive(false);
+                    restart.SetActive(false);
+                    home.SetActive(false);
+                    pause_text.SetActive(false);
                 }
                 break;
         }
--- Assets/Scripts/NewPlayer.cs
+++ Assets/Scripts/NewPlayer.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NewPlayer : MonoBehaviour
 {
 
     Rigidbody2D rd;
     float speed = 7f;
+    public int coins=0;
+    public Text text;
 
     public bool IsGround;
     Transform grounded;
@@ -17,6 +20,7 @@ public class NewPlayer : MonoBehaviour
     void Start()
     {
         rd = GetComponent<Rigidbody2D>();
+        Time.timeScale = 1;
 
         grounded = GameObject.Find(this.name + "/grounded").transform;
     }
@@ -25,10 +29,11 @@ public class NewPlayer : MonoBehaviour
     void Update()
     {
         IsGround = Physics2D.Linecast(transform.position, grounded.position, layerMask);
+        
     }
     void OnMouseDown()
     {
-        if (IsGround)
+        if (IsGround && Time.timeScale != 0)
         {
             Jump();
         }
@@ -46,8 +51,14 @@ public class NewPlayer : MonoBehaviour
 
         if (coll.transform.tag == "enemy")
         {
-
+            Time.timeScale = 0;
             Destroy(gameObject);
+        }
+        if(coll.transform.tag == "coins")
+        {
+            Destroy(coll.gameObject);
+            coins = coins + 1;
+            text.text = coins.ToString();
         }
 
     }
