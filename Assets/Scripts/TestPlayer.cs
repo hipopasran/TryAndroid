ed2e839494f5f87e413eb3f6202eb06752d4febd
hipopasran <vladimirkeda@yandex.ru>
2017-09-27 17:42:32 +0300

    bend of player

---
+++ Assets/Scripts/TestPlayer.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPlayer : MonoBehaviour {
+
+    Rigidbody2D rd;
+    public float speed = 7f;
+    public int coins;
+    Transform tr;
+
+    public float gravity = -9.8f;
+
+    public Transform telo;
+
+
+    public bool IsGround;
+    Transform grounded;
+    public LayerMask layerMask;
+
+    public int points = 0;
+   
+
+    
+
+
+    // Use this for initialization
+    void Start()
+    {
+        coins = PlayerPrefs.GetInt("Coins");
+        rd = GetComponent<Rigidbody2D>();
+        Time.timeScale = 1;
+        tr = GetComponent<Transform>();
+
+        grounded = GameObject.Find(this.name + "/grounded").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        IsGround = Physics2D.Linecast(transform.position, grounded.position, layerMask);
+
+       
+
+
+        if (rd.velocity == 0 * Vector2.up && IsGround == false)
+        {
+            rd.velocity = 2 * gravity * Vector2.up;
+            rd.gravityScale = 5;
+        }
+
+
+    }
+   
+    
+
+    void OnMouseDown()
+    {
+        StartCoroutine(Rot());
+
+    }
+
+    public void Jump()
+    {
+        AudioSource audio = GetComponent<AudioSource>();
+        audio.Play();
+
+        rd.velocity = speed * Vector2.up;
+    }
+
+    //void Rot()
+    //{
+    //    if (tr.rotation.z == 0)
+    //    {
+    //      tr.Rotate(Vector3.forward * -90);
+    //    }
+    //    Delay();
+    //    //else if (tr.rotation.z < 0)
+    //    //{ 
+    //        tr.Rotate(Vector3.forward * 90);
+    //    //}
+    //}
+
+    IEnumerator Rot()
+    {
+        Debug.Log(Time.time);
+        
+            telo.Rotate(Vector3.forward * -90);
+        
+        yield return new WaitForSeconds(1f);
+        //else if (tr.rotation.z < 0)
+        //{ 
+        telo.Rotate(Vector3.forward * 90);
+        //}
+        
+        
+        Debug.Log(Time.time);
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+
+        if (coll.transform.tag == "enemy")
+        {
+
+            
+           
+            Destroy(gameObject);
+
+        }
+    }
+
+    }
