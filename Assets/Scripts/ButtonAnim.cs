1c2600ed82378ee4d28f7ec52c596c26d8e51945
hipopasran <vladimirkeda@yandex.ru>
2017-10-13 06:08:45 +0300

    buttons anim

---
+++ Assets/Scripts/ButtonAnim.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonAnim : MonoBehaviour {
+
+    public Sprite layer_up, layer_down;
+
+    void OnMouseDown()
+    {
+        GetComponent<SpriteRenderer>().sprite = layer_down;
+    }
+
+    void OnMouseUp()
+    {
+        GetComponent<SpriteRenderer>().sprite = layer_up;
+    }
+	
+}
---
+++ Assets/Scripts/TextFade.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextFade : MonoBehaviour {
+
+    private Text txt;
+    private Outline oLine;
+
+	// Use this for initialization
+	void Start () {
+        txt = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        txt.color = new Color(txt.color.r, txt.color.g, txt.color.b, Mathf.PingPong(Time.time, 1.5f));
+
+	}
+}
